797f242c2d4f871ddb82aa4d61287458d0ac8f29
Karan Kadam <karank@outlook.com>
2024-09-08 11:24:24 +0930

    fix: 🐛 fix config paths

--- cmd/likho/main.go
+++ cmd/likho/main.go
@@ -2,31 +2,55 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/intothevoid/likho/internal/config"
 	"github.com/intothevoid/likho/internal/generator"
 	"github.com/intothevoid/likho/internal/server"
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 )
 
+func getConfig() *config.Config {
+	cfg, ok := viper.GetViper().Get("config").(*config.Config)
+	if !ok {
+		log.Fatal("Failed to parse configuration")
+	}
+	return cfg
+}
+
 func main() {
-	cfg, err := config.Load()
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")      // Look in current directory as well
+	viper.AddConfigPath("../../") // Look two directories up from cmd/likho
+	err := viper.ReadInConfig()
 	if err != nil {
 		fmt.Println("Error loading config:", err)
 		os.Exit(1)
 	}
+
+	var cfg config.Config
+	err = viper.Unmarshal(&cfg)
+	if err != nil {
+		log.Fatalf("Unable to decode into struct, %v", err)
+	}
+	viper.Set("config", &cfg)
 
 	rootCmd := &cobra.Command{
 		Use:   "likho",
 		Short: "Likho is a static site generator",
 	}
 
-	rootCmd.AddCommand(
-		createCmd(cfg),
-		generateCmd(cfg),
-		serveCmd(cfg),
-	)
+	confPtr, ok := viper.Get("config").(*config.Config)
+	if !ok {
+		log.Fatal("Failed to retrieve configuration")
+	}
+
+	rootCmd.AddCommand(createCmd(confPtr))
+	rootCmd.AddCommand(generateCmd(confPtr))
+	rootCmd.AddCommand(serveCmd(confPtr))
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
