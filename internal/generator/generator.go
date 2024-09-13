f7e754af81b75c95a90f929cbcd07378b4b806db
Karan Kadam <karank@outlook.com>
2024-09-13 21:49:25 +0930

    feat: 🎸 add index and all posts generation

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gomarkdown/markdown"
@@ -46,10 +47,20 @@ func Generate(cfg *config.Config) error {
 }
 
 func generateHTML(cfg *config.Config, posts []post.Post, about string, projects string) error {
-	// Parse all templates
-	tmpl, err := template.ParseGlob(filepath.Join(cfg.Content.TemplatesDir, "*.html"))
+	// Create a FuncMap with custom functions
+	funcMap := template.FuncMap{
+		"urlize": urlize,
+	}
+
+	// Parse all templates with the custom functions
+	tmpl, err := template.New("").Funcs(funcMap).ParseGlob(filepath.Join(cfg.Content.TemplatesDir, "*.html"))
 	if err != nil {
 		return fmt.Errorf("error parsing templates: %v", err)
+	}
+
+	// Generate index page
+	if err := generateIndexHTML(cfg, tmpl, posts); err != nil {
+		return err
 	}
 
 	// Generate post pages
@@ -69,7 +80,47 @@ func generateHTML(cfg *config.Config, posts []post.Post, about string, projects 
 		return err
 	}
 
+	// Generate all posts page
+	if err := generateAllPostsHTML(cfg, tmpl, posts); err != nil {
+		return err
+	}
+
 	return nil
+}
+
+func generateIndexHTML(cfg *config.Config, tmpl *template.Template, posts []post.Post) error {
+	data := struct {
+		Posts       []post.Post
+		SiteTitle   string
+		CurrentYear int
+		PageTitle   string
+		Content     template.HTML
+	}{
+		Posts:       posts,
+		SiteTitle:   cfg.Site.Title,
+		CurrentYear: time.Now().Year(),
+		PageTitle:   "Home",
+		Content:     "",
+	}
+
+	outputPath := filepath.Join(cfg.Content.OutputDir, "index.html")
+	return executeTemplate(tmpl, "index.html", outputPath, data)
+}
+
+// urlize converts a string to a URL-friendly format
+func urlize(s string) string {
+	// Convert to lowercase
+	s = strings.ToLower(s)
+	// Replace spaces with hyphens
+	s = strings.ReplaceAll(s, " ", "-")
+	// Remove any character that isn't a letter, number, or hyphen
+	s = strings.Map(func(r rune) rune {
+		if (r >= 'a' && r <= 'z') || (r >= '0' && r <= '9') || r == '-' {
+			return r
+		}
+		return -1
+	}, s)
+	return s
 }
 
 func generatePostHTML(cfg *config.Config, tmpl *template.Template, p post.Post) error {
@@ -82,14 +133,20 @@ func generatePostHTML(cfg *config.Config, tmpl *template.Template, p post.Post) 
 		Content     template.HTML
 		SiteTitle   string
 		CurrentYear int
+		PageTitle   string
 	}{
 		Post:        p,
 		Content:     template.HTML(html),
 		SiteTitle:   cfg.Site.Title,
 		CurrentYear: time.Now().Year(),
+		PageTitle:   p.Title,
 	}
 
-	outputPath := filepath.Join(cfg.Content.OutputDir, p.Slug+".html")
+	// Create the file name using both the title and the slug
+	fileName := fmt.Sprintf("%s-%s.html", p.Title, p.Slug)
+	fileName = strings.ToLower(strings.ReplaceAll(fileName, " ", "-"))
+	outputPath := filepath.Join(cfg.Content.OutputDir, fileName)
+
 	return executeTemplate(tmpl, "post.html", outputPath, data)
 }
 
@@ -98,10 +155,12 @@ func generateAboutHTML(cfg *config.Config, tmpl *template.Template, aboutContent
 		Content     template.HTML
 		SiteTitle   string
 		CurrentYear int
+		PageTitle   string
 	}{
 		Content:     template.HTML(aboutContent),
 		SiteTitle:   cfg.Site.Title,
 		CurrentYear: time.Now().Year(),
+		PageTitle:   "About",
 	}
 
 	outputPath := filepath.Join(cfg.Content.OutputDir, "about.html")
@@ -110,17 +169,40 @@ func generateAboutHTML(cfg *config.Config, tmpl *template.Template, aboutContent
 
 func generateProjectsHTML(cfg *config.Config, tmpl *template.Template, projectsContent string) error {
 	data := struct {
-		Projects    template.HTML
+		Posts       []post.Post
+		Content     template.HTML
 		SiteTitle   string
 		CurrentYear int
+		PageTitle   string
 	}{
-		Projects:    template.HTML(projectsContent),
+		Posts:       nil,
+		Content:     template.HTML(projectsContent),
 		SiteTitle:   cfg.Site.Title,
 		CurrentYear: time.Now().Year(),
+		PageTitle:   "Projects",
 	}
 
 	outputPath := filepath.Join(cfg.Content.OutputDir, "projects.html")
 	return executeTemplate(tmpl, "projects.html", outputPath, data)
+}
+
+func generateAllPostsHTML(cfg *config.Config, tmpl *template.Template, posts []post.Post) error {
+	data := struct {
+		Posts       []post.Post
+		SiteTitle   string
+		CurrentYear int
+		PageTitle   string
+		Content     template.HTML
+	}{
+		Posts:       posts,
+		SiteTitle:   cfg.Site.Title,
+		CurrentYear: time.Now().Year(),
+		PageTitle:   "All Posts",
+		Content:     "", // Leave empty as we're not using it directly
+	}
+
+	outputPath := filepath.Join(cfg.Content.OutputDir, "posts.html")
+	return executeTemplate(tmpl, "posts.html", outputPath, data)
 }
 
 func executeTemplate(tmpl *template.Template, name, outputPath string, data interface{}) error {
