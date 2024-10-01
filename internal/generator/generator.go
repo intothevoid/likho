5443477b2a91276e7f09363b72e41e7f31ad18b6
Karan Kadam <karank@outlook.com>
2024-10-01 21:46:22 +0930

    feat: 🎸 add sitemap and rss feed generation

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -297,12 +297,121 @@ func executeTemplate(tmpl *template.Template, name, outputPath string, data inte
 }
 
 func generateSitemap(cfg *config.Config, posts []post.Post) error {
-	// Implement sitemap generation logic
+	logger := utils.GetLogger()
+	sitemapPath := filepath.Join(cfg.Content.OutputDir, "sitemap.xml")
+	file, err := os.Create(sitemapPath)
+	if err != nil {
+		return fmt.Errorf("error creating sitemap file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+	if err != nil {
+		return fmt.Errorf("error writing sitemap header: %v", err)
+	}
+
+	_, err = file.WriteString("<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n")
+	if err != nil {
+		return fmt.Errorf("error writing urlset opening tag: %v", err)
+	}
+
+	// Add homepage URL
+	err = writeURL(file, cfg.Site.BaseURL, time.Now().Format("2006-01-02"))
+	if err != nil {
+		return err
+	}
+
+	// Add post URLs
+	for _, post := range posts {
+		postURL := fmt.Sprintf("%s/%s-%s.html", cfg.Site.BaseURL, urlize(post.Title), post.Slug)
+		err = writeURL(file, postURL, post.Date.Format("2006-01-02"))
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = file.WriteString("</urlset>")
+	if err != nil {
+		return fmt.Errorf("error writing urlset closing tag: %v", err)
+	}
+
+	logger.Info("sitemap generated", zap.String("path", sitemapPath))
 	return nil
 }
 
 func generateRSS(cfg *config.Config, posts []post.Post) error {
-	// Implement RSS feed generation logic
+	logger := utils.GetLogger()
+	rssPath := filepath.Join(cfg.Content.OutputDir, "rss.xml")
+	file, err := os.Create(rssPath)
+	if err != nil {
+		return fmt.Errorf("error creating RSS file: %v", err)
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n")
+	if err != nil {
+		return fmt.Errorf("error writing RSS header: %v", err)
+	}
+
+	_, err = file.WriteString("<rss version=\"2.0\">\n<channel>\n")
+	if err != nil {
+		return fmt.Errorf("error writing RSS channel opening tags: %v", err)
+	}
+
+	// Write channel information
+	_, err = fmt.Fprintf(file, "<title>%s</title>\n", cfg.Site.Title)
+	if err != nil {
+		return fmt.Errorf("error writing RSS title: %v", err)
+	}
+	_, err = fmt.Fprintf(file, "<link>%s</link>\n", cfg.Site.BaseURL)
+	if err != nil {
+		return fmt.Errorf("error writing RSS link: %v", err)
+	}
+	_, err = fmt.Fprintf(file, "<description>%s</description>\n", cfg.Site.Description)
+	if err != nil {
+		return fmt.Errorf("error writing RSS description: %v", err)
+	}
+
+	// Write items
+	for _, post := range posts {
+		_, err = file.WriteString("<item>\n")
+		if err != nil {
+			return fmt.Errorf("error writing item opening tag: %v", err)
+		}
+
+		_, err = fmt.Fprintf(file, "<title>%s</title>\n", post.Title)
+		if err != nil {
+			return fmt.Errorf("error writing item title: %v", err)
+		}
+
+		link := fmt.Sprintf("%s/%s-%s.html", cfg.Site.BaseURL, urlize(post.Title), post.Slug)
+		_, err = fmt.Fprintf(file, "<link>%s</link>\n", link)
+		if err != nil {
+			return fmt.Errorf("error writing item link: %v", err)
+		}
+
+		_, err = fmt.Fprintf(file, "<pubDate>%s</pubDate>\n", post.Date.Format(time.RFC1123Z))
+		if err != nil {
+			return fmt.Errorf("error writing item pubDate: %v", err)
+		}
+
+		_, err = fmt.Fprintf(file, "<description><![CDATA[%s]]></description>\n", post.Content)
+		if err != nil {
+			return fmt.Errorf("error writing item description: %v", err)
+		}
+
+		_, err = file.WriteString("</item>\n")
+		if err != nil {
+			return fmt.Errorf("error writing item closing tag: %v", err)
+		}
+	}
+
+	_, err = file.WriteString("</channel>\n</rss>")
+	if err != nil {
+		return fmt.Errorf("error writing RSS closing tags: %v", err)
+	}
+
+	logger.Info("rss feed generated", zap.String("path", rssPath))
 	return nil
 }
 
@@ -379,5 +488,13 @@ func generateTagPages(cfg *config.Config, posts []post.Post, pages []parser.Page
 		}
 	}
 
+	return nil
+}
+
+func writeURL(w io.Writer, loc string, lastmod string) error {
+	_, err := fmt.Fprintf(w, "  <url>\n    <loc>%s</loc>\n    <lastmod>%s</lastmod>\n  </url>\n", loc, lastmod)
+	if err != nil {
+		return fmt.Errorf("error writing URL to sitemap: %v", err)
+	}
 	return nil
 }
